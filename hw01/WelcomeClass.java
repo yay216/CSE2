4a1647914a4ff072c3adc2d23e2cc432bbd9ddd0
yay216 <yay216@lehigh.edu>
2014-09-03 01:08:03 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////
+//Yanxing Yang
+//WelcomeClass Java Program
+// first compile the program
+// javac WelcomeClass java
+//run the program
+// java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+//add main method
+public static void main(String[] args) {
+//print statements
+        System.out.println(" -----------");
+        System.out.println(" | WELCOME |");
+        System.out.println(" -----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-Y--A--Y--2--1--6-|");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println(" v  v  v  v  v  v");
+//print the tweet long sentence.
+System.out.println(" Today I dont feel like doing anything");
+    }
+}  
