c4a0f3fc42e44213a7bc315a4b461cb7f5adc96e
yay216 <yay216@lehigh.edu>
2014-09-03 00:56:30 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////
+//Yanxing Yang
+//HelloWorld Java Program
+// first compile the program
+// javac HelloWorld.java
+//run the program
+// java HelloWorld//
+
+//define a class
+public class HelloWorld{
+//add main method
+    public static void main(String[] args) {
+//print statements
+System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("#                         #");
+        System.out.println("#    Hello World 2014!!   #");
+        System.out.println("#                         #");
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("test");
+        //test/testtest
+    }
+}
